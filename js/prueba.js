82f62c0b57da6ecba7f71187bfd707e8ea8b2ca1
Damianotero11 <damianoteroar@gmail.com>
2022-02-28 17:34:45 -0300

    first commit

---
+++ js/prueba.js
@@ -0,0 +1,9 @@
+const dobles = 2;
+const triples = 3;
+const puntosAcumulados = puntos ++;
+
+
+var puntosTotales = (puntos)=>{
+    return puntosAcumulados + puntos;
+}
+console.log(puntosTotales(doble));
